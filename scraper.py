d00726dcf3c66465df0615410044dbbba95a329a
gerardomorales23 <39634874+gerardomorales23@users.noreply.github.com>
2020-03-30 17:13:56 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 #FECHA DE CREACION DE REPOSITORIO: 02 MARZO 2020
-#Ultima modificacion: 29 MARZO 2020
+#Ultima modificacion: 30 MARZO 2020
 import datetime
 from datetime import timedelta
 import pandas as pd
@@ -12,11 +12,13 @@ dia_f = int(input("Dia fin: "))
 mes_f = int(input("Mes fin: "))
 anio_f = int(input("Año fin: "))
 
+#Estan fijos estos datos para pruebas, pero la idea es que sean "inputs"
 sistema = "SIN"
 proceso = "MDA"
 nodo = "01PLO-115"
-formato = "JSON"
+formato = "JSON"   #Esta variable no debe ser modificada
 
+#Esta es la plantilla del URL del request
 url_base = "https://ws01.cenace.gob.mx:8082/SWPML/SIM/" + sistema + "/" + proceso + "/" + nodo + "/{}/{}/{}/{}/{}/{}/" + formato
 
 #convierte en formato de fecha los valores ingresados en el inicio
@@ -48,22 +50,28 @@ lista_3 = list()
 for i in range(0, int(dias_restantes)):
     lista_3.append(fecha_i + timedelta(days=7*rondas) + timedelta(days=i))
 
+#crea una lista con los urls del plazo del request
 lista_urls = list()
 for i in range(0, int(rondas)):
     lista_urls.append(url_base.format(lista_1[i].year, f'{lista_1[i].month:02}', f'{lista_1[i].day:02}', lista_2[i].year, f'{lista_2[i].month:02}', f'{lista_2[i].day:02}'))
 
 lista_urls.append(url_base.format(lista_3[0].year, f'{lista_3[0].month:02}', f'{lista_3[0].day:02}', lista_3[-1].year, f'{lista_3[-1].month:02}', f'{lista_3[-1].day:02}'))
 
+#Funcion que convierte el archivo JSON de cada URL en una Base de Datos (Data Frame)
 def url_into_df(url):
     json = pd.read_json(url)["Resultados"][0]["Valores"]
     data_frame  = pd.DataFrame(json)
     
     return data_frame 
 
+#Data Frame vacío donde se almacernara la información de los PMLs descargados
 df = pd.DataFrame()
 
+#crea un Data Frame por cada URL y lo concatena en un solo Data Frame
 for urlz in lista_urls:
     df_nuevo = url_into_df(urlz)
     df = pd.concat((df, df_nuevo))
 
+#Crea un documento de excel con la información de la consulta
+#En Jupyter Notebooks funciono sin problema, Visual Studio Code no genera el archivo de Excel
 df.to_excel("ConsultaPML.xlsx")
