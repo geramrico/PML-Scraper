d6f8952e80d6c73b80984c6eb4c6b8f9816e40a0
gerardomorales23 <39634874+gerardomorales23@users.noreply.github.com>
2020-04-20 17:42:02 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,13 @@
-#FECHA DE CREACION DE REPOSITORIO: 02 MARZO 2020
-#Ultima modificacion: 30 MARZO 2020
+### To Do's ###
+# - Preguntar donde almacenar consulta
+# Por default que se guarde en Desktop
+# Hacer una GUI para seleccionar más facil la info
+# auto generacion de reporte sobre rango de consulta
+
 import datetime
 from datetime import timedelta
 import pandas as pd
+import re
 
 dia_i = int(input("Dia inicio: "))
 mes_i = int(input("Mes inicio: "))
@@ -12,16 +17,52 @@ dia_f = int(input("Dia fin: "))
 mes_f = int(input("Mes fin: "))
 anio_f = int(input("Año fin: "))
 
-#Estan fijos estos datos para pruebas, pero la idea es que sean "inputs"
-sistema = "SIN"
-proceso = "MDA"
-nodo = "01PLO-115"
-formato = "JSON"   #Esta variable no debe ser modificada
 
-#Esta es la plantilla del URL del request
-url_base = "https://ws01.cenace.gob.mx:8082/SWPML/SIM/" + sistema + "/" + proceso + "/" + nodo + "/{}/{}/{}/{}/{}/{}/" + formato
+while True:
+    sistema = input("SIN BCA o BCS: ").upper()
+    if sistema in ("SIN","BCA","BCS"):
+        break
+    else:
+        continue
 
-#convierte en formato de fecha los valores ingresados en el inicio
+while True:
+    proceso = input("MDA o MTR: ").upper()
+    if proceso in ("MDA","MTR"):
+        break
+    else:
+        continue
+
+while True:
+    tipo_nodo = input("Nodo P o D: ").upper()
+    if tipo_nodo in ("P","D"):
+        break
+    else:
+        continue
+
+if tipo_nodo == "D":
+    nodo = input("Introduce nombre nodo: ").upper()
+else:
+    while True:
+        nodo = input("Inserta NodoP: ").upper()
+        if re.match('^[0-9]{2}[A-Z]{3}-[0-9]{3}$', nodo):
+            break
+        else:
+            print("El formato debe ser ##XXX-###")
+            continue
+
+
+url_base = (
+    "https://ws01.cenace.gob.mx:8082/SWPML/SIM/"
+    + sistema
+    + "/"
+    + proceso
+    + "/"
+    + nodo
+    + "/{}/{}/{}/{}/{}/{}/"
+    + "JSON"
+)
+
+# convierte en formato de fecha los valores ingresados en el inicio
 fecha_i = datetime.datetime(anio_i, mes_i, dia_i)
 fecha_f = datetime.datetime(anio_f, mes_f, dia_f)
 
@@ -31,47 +72,66 @@ plazo = (fecha_f - fecha_i).days + 1
 # si el plazo es menor a una semana, solo hará 1 ronda (consulta minima es de 1 semana)
 if plazo < 7:
     rondas = 1
-# si el plazo es mayor a 7 días, se asignan rondas = al ultimo numero multiplo de 7 en el plazo
+    # si el plazo es mayor a 7 días, se asignan rondas = al ultimo numero multiplo de 7 en el plazo
 else:
     rondas = (plazo - plazo % 7) / 7
 
-# con eso cubres los dias que no entran en las rondas (si plazo = 31, rondas = 4, dias_restantes = 3)
+    # con eso cubres los dias que no entran en las rondas (si plazo = 31, rondas = 4, dias_restantes = 3)
 dias_restantes = plazo - rondas * 7
 
 # crea listas de "inicio" y "fin" de los rangos de busqueda
 lista_1, lista_2 = list(), list()
 
 for i in range(0, int(rondas)):
-    lista_1.append((fecha_i + timedelta(days=(7*i))))
-    lista_2.append((fecha_i + timedelta(days=(7*i)) + timedelta(days=6)))
+    lista_1.append((fecha_i + timedelta(days=(7 * i))))
+    lista_2.append((fecha_i + timedelta(days=(7 * i)) + timedelta(days=6)))
 
-# considera los "dias restantes" que quedan fuera del rango
+    # considera los "dias restantes" que quedan fuera del rango
 lista_3 = list()
 for i in range(0, int(dias_restantes)):
-    lista_3.append(fecha_i + timedelta(days=7*rondas) + timedelta(days=i))
+    lista_3.append(fecha_i + timedelta(days=7 * rondas) + timedelta(days=i))
 
-#crea una lista con los urls del plazo del request
 lista_urls = list()
 for i in range(0, int(rondas)):
-    lista_urls.append(url_base.format(lista_1[i].year, f'{lista_1[i].month:02}', f'{lista_1[i].day:02}', lista_2[i].year, f'{lista_2[i].month:02}', f'{lista_2[i].day:02}'))
+    lista_urls.append(
+        url_base.format(
+            lista_1[i].year,
+            f"{lista_1[i].month:02}",
+            f"{lista_1[i].day:02}",
+            lista_2[i].year,
+            f"{lista_2[i].month:02}",
+            f"{lista_2[i].day:02}",
+        )
+    )
 
-lista_urls.append(url_base.format(lista_3[0].year, f'{lista_3[0].month:02}', f'{lista_3[0].day:02}', lista_3[-1].year, f'{lista_3[-1].month:02}', f'{lista_3[-1].day:02}'))
+lista_urls.append(
+    url_base.format(
+        lista_3[0].year,
+        f"{lista_3[0].month:02}",
+        f"{lista_3[0].day:02}",
+        lista_3[-1].year,
+        f"{lista_3[-1].month:02}",
+        f"{lista_3[-1].day:02}",
+    )
+)
 
-#Funcion que convierte el archivo JSON de cada URL en una Base de Datos (Data Frame)
+
 def url_into_df(url):
     json = pd.read_json(url)["Resultados"][0]["Valores"]
-    data_frame  = pd.DataFrame(json)
-    
-    return data_frame 
+    data_frame = pd.DataFrame(json)
 
-#Data Frame vacío donde se almacernara la información de los PMLs descargados
+    return data_frame
+
+
 df = pd.DataFrame()
 
-#crea un Data Frame por cada URL y lo concatena en un solo Data Frame
 for urlz in lista_urls:
     df_nuevo = url_into_df(urlz)
     df = pd.concat((df, df_nuevo))
 
-#Crea un documento de excel con la información de la consulta
-#En Jupyter Notebooks funciono sin problema, Visual Studio Code no genera el archivo de Excel
-df.to_excel("ConsultaPML.xlsx")
+
+df.to_csv(
+    "{} {}.{}.{} - {}.{}.{}.csv".format(
+        nodo, anio_i, mes_i, dia_i, anio_f, mes_f, dia_f
+    )
+)
